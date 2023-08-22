ea71eea92109ca847b958d9e4ce491e7d80ee817
mampiononavoahary <hei.zoarisoa@gmail.com>
2023-08-22 20:14:28 +0300

    feat: add service for insert update and select all

--- SpringBootProject/src/main/java/com/example/springbootproject/Service/EmployeeService.java
+++ SpringBootProject/src/main/java/com/example/springbootproject/Service/EmployeeService.java
@@ -1,2 +1,37 @@
-package com.example.springbootproject.Service;public class EmployeeService {
+package com.example.springbootproject.Service;
+
+import com.example.springbootproject.model.Employee;
+import com.example.springbootproject.repository.EmployeeDAO;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+    private EmployeeDAO empDAO;
+
+    public EmployeeService(EmployeeDAO empDAO) {
+        this.empDAO = empDAO;
+    }
+    public Employee insert(Employee insertEmp){
+        try{
+            this.empDAO.InsertEmployee(insertEmp);
+
+            return insertEmp;
+        }catch (Exception e){
+            throw new RuntimeException("an error survenu");
+        }
+    }
+    public List<Employee> findAllEmployee() throws Exception{
+        return empDAO.FindAllEmp();
+    }
+    public Optional<Employee> findByid(int id_employee){
+        try {
+            return empDAO.findEmployeeById(id_employee);
+        } catch (SQLException e) {
+            throw new RuntimeException("There has been an error when fetching todo with id :"+id_employee);
+        }
+    }
 }
